3e1ae93be87f751d02d1f5e9aaffa3e91d20727b
vuongvu <hoangvuong.v99@gmail.com>
2019-04-09 13:36:59 +0700

    Separate Login component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
-import Home from '../home';
+import Upload from '../upload';
 import About from '../about';
+import Login from '../login';
 import './index.scss';
 
 const App = () => (
   <div>
     <header>
-      <Link to="/">Home</Link>
+      <Link to="/">Upload</Link>
       <Link to="/about-us">About</Link>
+      <Login />
     </header>
 
     <main>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={Upload} />
       <Route exact path="/about-us" component={About} />
     </main>
   </div>
---
+++ src/components/login/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { authenticate } from 'actions/nuxeo';
+
+class Login extends React.Component {
+  render() {
+    const { nuxeo, authenticate } = this.props;
+    return (
+      <>
+        {
+          nuxeo
+          ? (
+            <div>Login as {nuxeo._auth.username}</div>
+          )
+          : (<button onClick={() => authenticate()}>Login</button>)
+        }
+      </>
+    );
+  }
+};
+
+const mapStateToProps = state => ({
+  nuxeo: state.nuxeo.authentication,
+});
+
+const mapDispatchToProps = dispatch => ({
+  authenticate: () => dispatch(authenticate()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login)
